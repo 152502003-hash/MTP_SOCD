4c2fd6d354e6dbdde1e3084f7fe91f96314ebdcc
152502003-hash <152502003@smail.iitpkd.ac.in>
2026-08-21 20:33:35 +0530

    Create tb.cpp

---
+++ tb.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "ap_int.h"
+
+#define N 4
+
+void matrix_mul(
+    ap_int<16> A[N][N],
+    ap_int<16> B[N][N],
+    ap_int<32> C[N][N]
+);
+
+int main()
+{
+    ap_int<16> A[N][N];
+    ap_int<16> B[N][N];
+    ap_int<32> C[N][N];
+
+    // Initialize matrices
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            A[i][j] = i + j + 1;
+            B[i][j] = i + j + 1;
+            C[i][j] = 0;
+        }
+    }
+
+    // Call matrix multiplication
+    matrix_mul(A, B, C);
+
+    // Print result
+    std::cout << "Matrix C = A x B\n";
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            std::cout << C[i][j] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
