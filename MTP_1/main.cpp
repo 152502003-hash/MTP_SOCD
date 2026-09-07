9964ae99e13ce54fbfe06ba2a8bce7f20f15e703
152502003-hash <152502003@smail.iitpkd.ac.in>
2026-09-07 10:05:40 +0530

    Update main.cpp

--- MTP_1/main.cpp
+++ MTP_1/main.cpp
@@ -1,1 +1,26 @@
 
+#include "ap_int.h"
+
+#define N 4
+
+void matrix_mul(
+    ap_int<16> A[N][N],
+    ap_int<16> B[N][N],
+    ap_int<32> C[N][N]
+)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            ap_int<32> sum = 0;
+
+            for (int k = 0; k < N; k++)
+            {
+                sum = sum + A[i][k] * B[k][j];
+            }
+
+            C[i][j] = sum;
+        }
+    }
+}
